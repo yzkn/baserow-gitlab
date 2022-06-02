86232c3a608850505fe95d03cfbea8e3d5b6c111
alex1431999 <alex@baserow.io>
2022-06-02 13:24:13 +0200

    Add simple performance test

--- backend/tests/baserow/contrib/database/table/test_table_usage_types.py
+++ backend/tests/baserow/contrib/database/table/test_table_usage_types.py
@@ -1,4 +1,5 @@
 import pytest
+from pyinstrument import Profiler
 
 from baserow.contrib.database.rows.handler import RowHandler
 from baserow.contrib.database.table.usage_types import TableGroupStorageUsageItemType
@@ -35,3 +36,43 @@ def test_table_group_storage_usage_item_type(data_fixture):
     usage = TableGroupStorageUsageItemType().calculate_storage_usage(group.id)
 
     assert usage == 700
+
+
+@pytest.mark.django_db
+@pytest.mark.disabled_in_ci
+# You must add --run-disabled-in-ci -s to pytest to run this test, you can do this in
+# intellij by editing the run config for this test and adding --run-disabled-in-ci -s
+# to additional args.
+def test_table_group_storage_usage_item_type_performance(data_fixture):
+    files_amount = 1000
+    file_size_each = 200
+
+    user = data_fixture.create_user()
+    group = data_fixture.create_group(user=user)
+    database = data_fixture.create_database_application(group=group)
+    table = data_fixture.create_database_table(user=user, database=database)
+    file_field = data_fixture.create_file_field(table=table)
+
+    user_files = [
+        {
+            f"field_{file_field.id}": [
+                {
+                    "name": data_fixture.create_user_file(
+                        is_image=True, size=file_size_each
+                    ).name
+                }
+            ]
+        }
+        for i in range(files_amount)
+    ]
+
+    RowHandler().create_rows(user, table, user_files)
+
+    profiler = Profiler()
+    profiler.start()
+    usage = TableGroupStorageUsageItemType().calculate_storage_usage(group.id)
+    profiler.stop()
+
+    print(profiler.output_text(unicode=True, color=True))
+
+    assert usage == files_amount * file_size_each
